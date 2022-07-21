3a756c765ff50e8449e9664cfff1efa6cdb16a31
Henrique-dev-git <carlos.henrique.psic@gmail.com>
2022-07-21 19:27:35 -0300

    exercicios prontos

---
+++ exercicio1/script.js
@@ -0,0 +1,5 @@
+let resposta = prompt ("insira um número par");
+let respostaNumber = Number(resposta)
+console.log(respostaNumber%2)
+//quando inserido um número par a resposta é sempre 0
+//quando inserido um número impar o resultado será sempre 1 
---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+
+let n1 = prompt ("digite um número");
+let n2 = prompt ("digite outro número");
+n1 = Number
+n2 = Number
+
+console.log(n1>n2)
+console.log(n1==n2)
+console.log((n1%n2)==0)
+console.log((n2%n1)==0)
